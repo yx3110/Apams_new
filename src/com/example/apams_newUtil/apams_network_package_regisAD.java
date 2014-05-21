4ebde2c223e0670d48f772f78f24903dc18a4a3b
yx3110 <eclipse.myworld@gmail.com>
2014-05-21 19:24:55 +0100

    {++

---
+++ src/com/example/apams_newUtil/apams_network_package_regisAD.java
@@ -0,0 +1,10 @@
+package com.example.apams_newUtil;
+
+public class apams_network_package_regisAD extends apams_network_package {
+
+	public apams_network_package_regisAD(String username, String password,
+			String CID) {
+		super(username, password, CID, packageType.REGISTER_AD);
+	}
+
+}
