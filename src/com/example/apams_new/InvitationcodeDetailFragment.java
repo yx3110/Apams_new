1cdf1d1d97a295c346bbb53aeb09aa4b331c175a
yx3110 <eclipse.myworld@gmail.com>
2014-06-04 17:33:30 +0100

    6.4 invite detail

--- src/com/example/apams_new/InvitationcodeDetailFragment.java
+++ src/com/example/apams_new/InvitationcodeDetailFragment.java
@@ -1,17 +1,25 @@
 package com.example.apams_new;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.apams_newUtil.InviteInfo;
+import com.example.apams_newUtil.QRencoder;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 
 /**
  * A fragment representing a single Invitation code detail screen. This fragment
@@ -24,6 +32,7 @@ public class InvitationcodeDetailFragment extends Fragment {
 	 * represents.
 	 */
 	public static final String ARG_ITEM_ID = "item_id";
+	private static final String IMAGE_FORMAT = "png";
 
 	/**
 	 * The dummy content this fragment is presenting.
@@ -36,22 +45,24 @@ public class InvitationcodeDetailFragment extends Fragment {
 	 */
 	public InvitationcodeDetailFragment() {
 	}
+
 	private ArrayList<InviteInfo> inviteList;
-	private HashMap<String,InviteInfo> inviteMap;
-	
+	private HashMap<String, InviteInfo> inviteMap;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Intent intent = this.getActivity().getIntent();
-		this.inviteList = (ArrayList<InviteInfo>) intent.getSerializableExtra("inviteList");
-		this.inviteMap = (HashMap<String,InviteInfo>) intent.getSerializableExtra("inviteMap");
+		this.inviteList = (ArrayList<InviteInfo>) intent
+				.getSerializableExtra("inviteList");
+		this.inviteMap = (HashMap<String, InviteInfo>) intent
+				.getSerializableExtra("inviteMap");
 
 		if (getArguments().containsKey(ARG_ITEM_ID)) {
 			// Load the dummy content specified by the fragment
 			// arguments. In a real-world scenario, use a Loader
 			// to load content from a content provider.
-			mItem = inviteMap.get(getArguments().getString(
-					ARG_ITEM_ID));
+			mItem = inviteMap.get(getArguments().getString(ARG_ITEM_ID));
 		}
 	}
 
@@ -63,8 +74,27 @@ public class InvitationcodeDetailFragment extends Fragment {
 
 		// Show the dummy content as text in a TextView.
 		if (mItem != null) {
-			((TextView) rootView.findViewById(R.id.invitationcode_detail))
-					.setText(mItem.toString());
+			ImageButton imgb = (ImageButton) rootView
+					.findViewById(R.id.invite_detail_qr);
+			try {
+				Bitmap bitmap = QRencoder.encodeAsBitmap(mItem.getCode(), "",
+						BarcodeFormat.QR_CODE, imgb.getLayoutParams().width,
+						imgb.getLayoutParams().height);
+				imgb.setImageBitmap(bitmap);
+			} catch (WriterException e) {
+				e.printStackTrace();
+			}
+			
+			TextView belonglvl = (TextView) rootView.findViewById(R.id.invite_belongandlvl);
+			TextView date = (TextView) rootView.findViewById(R.id.invite_detail_date);
+			TextView activation = (TextView) rootView.findViewById(R.id.invite_activation);
+			belonglvl.setText(mItem.getBelongto()+mItem.getLevel());
+			date.setText("Date of issue: "+mItem.getTime());
+			String yes = mItem.getActivated()?"Yes":"No";
+			String activatedBy = mItem.getActivated()?mItem.getActivatedBy():"N/A";
+			activation.setText("Activated: "+ yes+" Activated by: "+activatedBy);
+
+
 		}
 
 		return rootView;
---
+++ src/com/example/apams_newUtil/QRencoder.java
@@ -0,0 +1,40 @@
+package com.example.apams_newUtil;
+
+import java.util.Hashtable;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+import android.graphics.Bitmap;
+
+public class QRencoder {
+	public static Bitmap encodeAsBitmap(String contents,String hint, BarcodeFormat format,
+			int desiredWidth, int desiredHeight) throws WriterException {
+		Hashtable<EncodeHintType, Object> hints = null;
+		if (hint != null) {
+			hints = new Hashtable<EncodeHintType, Object>(2);
+			hints.put(EncodeHintType.CHARACTER_SET, hint);
+		}
+		MultiFormatWriter writer = new MultiFormatWriter();
+		BitMatrix result = writer.encode(contents, format, desiredWidth,
+				desiredHeight, hints);
+		int width = result.getWidth();
+		int height = result.getHeight();
+		int[] pixels = new int[width * height];
+		// All are 0, or black, by default
+		for (int y = 0; y < height; y++) {
+			int offset = y * width;
+			for (int x = 0; x < width; x++) {
+				pixels[offset + x] = result.get(x, y) ? 0 : 1;
+			}
+		}
+
+		Bitmap bitmap = Bitmap.createBitmap(width, height,
+				Bitmap.Config.ARGB_8888);
+		bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+		return bitmap;
+	}
+}
