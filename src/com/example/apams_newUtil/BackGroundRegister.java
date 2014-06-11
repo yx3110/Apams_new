f3034d5bc24ce98d87c764a4804a764aa3fa8524
yx3110 <eclipse.myworld@gmail.com>
2014-06-11 20:21:01 +0100

    leave it

--- src/com/example/apams_newUtil/BackGroundRegister.java
+++ src/com/example/apams_newUtil/BackGroundRegister.java
@@ -142,7 +142,6 @@ public class BackGroundRegister extends Thread {
 						StrOut.write("UPDATEDONE");
 						StrOut.flush();
 						StrOut.close();
-						socket.close();
 						break;
 					case FINDPW:
 						System.out.println("Package type = " + pack.getType());
